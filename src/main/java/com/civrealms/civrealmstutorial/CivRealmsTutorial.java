dae03e2236a2759c248d8171f0de3be35fff96e1
crimeo <gavin.jenkins@BB0000041.office.bluebatgames.com>
2019-02-06 16:10:48 -0800

    Revert "Revert "Initial Commit""
    This reverts commit f2bd01386d2ebc83314aa44490295a5979568d68.

---
+++ src/main/java/com/civrealms/civrealmstutorial/CivRealmsTutorial.java
@@ -0,0 +1,13 @@
+/*
+ * This project is licensed under GPLv3
+ * https://www.gnu.org/licenses/gpl-3.0.html
+ */
+package com.civrealms.civrealmstutorial;
+
+/**
+ *
+ * @author crimeo
+ */
+public class CivRealmsTutorial {
+    
+}
